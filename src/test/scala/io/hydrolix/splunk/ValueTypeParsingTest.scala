86d1178e015756b04b69b1df5dea1d9993b1772a
Alex Cruise <alex@hydrolix.io>
2023-10-19 15:48:29 -0700

    Bump connectors-core to 0.3.2-SNAPSHOT to get partition pruning optz; add mystery test

---
+++ src/test/scala/io/hydrolix/splunk/ValueTypeParsingTest.scala
@@ -0,0 +1,13 @@
+package io.hydrolix.splunk
+
+import org.junit.{Ignore, Test}
+
+import io.hydrolix.connectors.types.ValueType
+
+class ValueTypeParsingTest {
+  @Ignore("doesn't work, don't know why")
+  @Test
+  def FAFO(): Unit = {
+    println(ValueType.parse("""struct<"timestamp":timestamp(3),"level":string,"message":string>"""))
+  }
+}
